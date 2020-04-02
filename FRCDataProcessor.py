26e90cc63296db5e3ab6c448b3732e4862492907
ksam <samchoks@hotmail.com>
2020-04-02 14:44:28 -0400

    Finished moving over to Pandas. Made constructor more robust. Added helper methods. Optimized runtime for collectMatchesDataFrame

--- FRCDataProcessor.py
+++ FRCDataProcessor.py
@@ -5,30 +5,46 @@ from scipy.sparse.linalg import lsmr
 from SparseMatrixConstructor import SparseMatrix
 import math
 import pandas as pd
+from pathlib import Path
 
 
 class FRCDataProcessor(object):
-    def __init__(self, authKey, year, data=None):
+    def __init__(self, authKey, year, data=None, teamList=None):
         self.headers = {
             'accept': 'application/json',
             'X-TBA-Auth-Key': authKey,
         }
         self.year = year
+
+        d = Path("MatchData/data" + str(year) + ".pkl.xz")
+        if data is not None:
+            self.data = data
+        elif data is None and d.is_file():
+            self.data = pd.read_pickle("MatchData/data" + str(year) + ".pkl.xz")
+        else:
+            self.data = pd.DataFrame()
+
+        t = Path("Results/SortedTeamList" + str(year) + ".txt")
+        if teamList is not None:
+            self.teamList = teamList
+        elif t.is_file():
+            self.readTeamListfromtxt(year)
+        else:
+            self.teamList = pd.Series()
+
         self.numberOfMatches = None
-        self.numberOfTeams = None
+        self.numberOfTeams = self.teamList.shape[0]
         self.numberOfFiles = None
-        self.numberOfValidMatches = None
+        self.numberOfValidMatches = self.data.shape[0]
         self.percentageOfValidMatches = None
-        self.teamList = None
-        self.data = data
+        self.currentXPR = None
 
     def collectMatchesDataFrame(self):
         totalMatches = 0
         validMatches = 0
-        fileCounter = 0
-        total_match_list = pd.DataFrame()
+        total_match_list = {}
         team_list = []
-        Events_response = requests.get('https://www.thebluealliance.com/api/v3/events/' + self.year + '/keys',
+        Events_response = requests.get('https://www.thebluealliance.com/api/v3/events/' + str(self.year) + '/keys',
                                        headers=self.headers)
         Events_json = Events_response.json()
         for i in range(len(Events_json)):
@@ -37,32 +53,32 @@ class FRCDataProcessor(object):
                 'https://www.thebluealliance.com/api/v3/event/' + Events_json[i] + '/matches', headers=self.headers
             ).json()
             totalMatches += len(eventMatches)
-            if len(eventMatches) > 0 and eventMatches[0]["score_breakdown"] is not None:
-                fileCounter += 1
-                validMatches += len(eventMatches)
+            if len(eventMatches) > 0:
                 for event_match in eventMatches:
-                    # match_list[event_match["key"]] = event_match
-                    match_list =  pd.json_normalize(event_match)
-                    total_match_list = total_match_list.append(match_list)
-                    #match_list.to_pickle("MatchData/Matches/"+event_match["key"]+".pkl.xz", compression="infer")
-                    for team in event_match["alliances"]["blue"]["team_keys"]:
-                        team_list.append(team)
-                    for team in event_match["alliances"]["red"]["team_keys"]:
-                        team_list.append(team)
-                    team_list = list(dict.fromkeys(team_list))
-        total_match_list.to_pickle("MatchData/data2020.pkl.xz", compression="infer")
-        #total_match_list.to_csv("MatchData/data.csv")
+                    if event_match["score_breakdown"] is not None:
+                        validMatches += 1
+                        # match_list[event_match["key"]] = event_match
+                        match = self.flatten_json(event_match)
+                        total_match_list[match["key"]] = match
+                        # match_list.to_pickle("MatchData/Matches/"+event_match["key"]+".pkl.xz", compression="infer")
+                        for team in event_match["alliances"]["blue"]["team_keys"]:
+                            team_list.append(team)
+                        for team in event_match["alliances"]["red"]["team_keys"]:
+                            team_list.append(team)
+                        team_list = list(dict.fromkeys(team_list))
+        print("Creating Dataframe")
+        pdMatchList = pd.DataFrame.from_dict(total_match_list, orient="rows")
+        pdMatchList.to_pickle("MatchData/data" + str(self.year) + ".pkl.xz", compression="infer")
+        # total_match_list.to_csv("MatchData/data.csv")
         team_list.sort()
-        with open('Results/SortedTeamList' + self.year + '.txt', 'w') as f:
+        with open('Results/SortedTeamList' + str(self.year) + '.txt', 'w') as f:
             for item in team_list:
                 f.write("%s\n" % item)
-        self.data = total_match_list
+        self.data = pdMatchList
         self.numberOfMatches = totalMatches
-        self.numberOfFiles = math.ceil(fileCounter / 2)
-        print(self.numberOfFiles)
         self.numberOfValidMatches = validMatches
         self.percentageOfValidMatches = round(validMatches / totalMatches, 2)
-        self.teamList = team_list
+        self.teamList = pd.Series(team_list)
         self.numberOfTeams = len(team_list)
 
     def collectMatchesJSON(self):
@@ -108,34 +124,28 @@ class FRCDataProcessor(object):
         self.teamList = team_list
         self.numberOfTeams = len(team_list)
 
-    def provideMatches(self, numberOfValidMatches, numberOfTeams, numberOfFiles, teamList):
-        self.numberOfValidMatches = numberOfValidMatches
-        self.numberOfTeams = numberOfTeams
-        self.numberOfFiles = numberOfFiles
-        self.teamList = teamList
-
     def XPR(self, x):
         sparseA = SparseMatrix((self.numberOfValidMatches * 2, len(self.teamList)), np.int32)
         sparseB = SparseMatrix((self.numberOfValidMatches * 2, 1), np.int32)
-        for i in range(1, self.numberOfFiles):
-            with open("MatchData/MatchData" + self.year + "/" + str(i) + ".0.json", newline='') as r:
-                readfile = json.load(r)
-                u = 0
-                for match in readfile:
-                    match_blue_teams = [*readfile[match]["alliances"]["blue"]["team_keys"]]
-                    match_red_teams = [*readfile[match]["alliances"]["red"]['team_keys']]
-                    sparseB.append(2 * u, 0, readfile[match]["score_breakdown"]["blue"][x])
-                    sparseB.append(2 * u + 1, 0, readfile[match]["score_breakdown"]["red"][x])
-                    for team in match_blue_teams:
-                        sparseA.append(2 * u, self.teamList.index(team), 1)
-                    for team in match_red_teams:
-                        sparseA.append(2 * u + 1, self.teamList.index(team), 1)
-                    u += 1
+        for index, row in self.data.iterrows():  # TODO: Replace with Splicing Columns
+            u = 0
+            match_blue_teams = row["alliances.blue.team_keys"]
+            match_red_teams = row["alliances.red.team_keys"]
+            print(row["key"])
+            sparseB.append(2 * u, 0, int(row["score_breakdown.blue." + x]))
+            sparseB.append(2 * u + 1, 0, int(row["score_breakdown.red." + x]))
+            for team in match_blue_teams:
+                sparseA.append(2 * u, self.findTeam(team), 1)
+            for team in match_red_teams:
+                sparseA.append(2 * u + 1, self.findTeam(team), 1)
+            u += 1
 
         A = sparseA.tocoo().tocsr()
         B = np.ndarray.flatten(sparseB.tocoo().tocsc().toarray())
 
         ans = lsmr(A, B)[0]
+        self.currentXPR = pd.Series(ans)
+        print(self.currentXPR)
         np.savetxt("Results/" + x + "PR.txt", ans)
 
     def PCA(self, numberOfComponents):
@@ -143,5 +153,35 @@ class FRCDataProcessor(object):
 
     @staticmethod
     def readpkl():
-        df = pd.read_pickle("MatchData/data.pkl.xz",compression="infer")
+        df = pd.read_pickle("MatchData/data2019.pkl.xz", compression="infer")
         print(df)
+
+    def readTeamListfromtxt(self, year):
+        teamList = []
+        with open("Results/SortedTeamList" + str(year) + ".txt") as f:
+            for line in f:
+                teamList.append(line.strip())
+        self.teamList = pd.Series(teamList)
+        self.teamList.to_pickle("Results/SortedTeamList" + str(year) + ".pkl.xz")
+
+    def findTeam(self, team):
+        return list(self.teamList[self.teamList == team].index.values)[0]
+
+    @staticmethod
+    def flatten_json(y):
+        out = {}
+
+        def flatten(x, name=''):
+            if type(x) is dict:
+                for a in x:
+                    flatten(x[a], name + a + '_')
+            elif type(x) is list:
+                i = 0
+                for a in x:
+                    flatten(a, name + str(i) + '_')
+                    i += 1
+            else:
+                out[name[:-1]] = x
+
+        flatten(y)
+        return out
--- Predictor.py
+++ Predictor.py
@@ -1,4 +1,5 @@
 from FRCDataProcessor import FRCDataProcessor
 
-P = FRCDataProcessor("C6StJN6kweYGKqhV5iQURbWX2vRudwcjJYwBobcxINlbAKurqR35lRQsHwc9eQ5c", "2020")
+# authkey is C6StJN6kweYGKqhV5iQURbWX2vRudwcjJYwBobcxINlbAKurqR35lRQsHwc9eQ5c
+P = FRCDataProcessor("C6StJN6kweYGKqhV5iQURbWX2vRudwcjJYwBobcxINlbAKurqR35lRQsHwc9eQ5c", 2019)
 P.collectMatchesDataFrame()
